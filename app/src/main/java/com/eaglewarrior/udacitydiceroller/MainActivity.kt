22bd935e509f43575758e024a731f54a168fbe01
AndroidLion48 <>
2022-11-16 13:31:37 -0800

    binding button view

--- app/src/main/java/com/eaglewarrior/udacitydiceroller/MainActivity.kt
+++ app/src/main/java/com/eaglewarrior/udacitydiceroller/MainActivity.kt
@@ -2,10 +2,23 @@ package com.eaglewarrior.udacitydiceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollbtn: Button = findViewById(R.id.roll_btn)
+        rollbtn.setOnClickListener{
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val resultText: TextView = findViewById(R.id.dice_tv)
+        resultText.text = "Dice Roll"
     }
 }
